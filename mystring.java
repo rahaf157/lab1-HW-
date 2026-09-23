dff5127a514c5ab92038f2264d3f0efa3e687eea
rahaf157 <s2221161728@ku.edu.kw>
2026-09-23 21:31:43 +0300

    the change directly on GitHub.

--- mystring.java
+++ mystring.java
@@ -13,7 +13,7 @@ public class mystring {
  public  String  invert( ){ 
   int i , n = Text.length(); 
    
-  char []  Temp; 
+  char []  Temp= new char [n+1];
   Temp = Text.toCharArray(); 
   Text= ""; 
   for ( i=0; i<n; i++) { 
